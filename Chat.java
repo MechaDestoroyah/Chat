e27b2b531bdea780ccb0777b22e77dea43a5b098
MechaDestoroyah <codymay@mail.weber.edu>
2017-02-13 19:08:07 -0700

    Update Chat.java

--- Chat.java
+++ Chat.java
@@ -1,68 +1,141 @@
 package Chat;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.*;
 import java.util.*;
-//import java.util.List;
+import javax.swing.*;
 
-public class Chat {
-	public static List<Group> Groups= new ArrayList<Group>();
-	public static List<Student> Students = new ArrayList<Student>();
+
+public class Chat extends JFrame{
+	
+	private static final long serialVersionUID = -5394960899104203569L;
+	public static List<Group> groups = new ArrayList<Group>();
+	public static List<Student> students = new ArrayList<Student>();
+	private JFrame frame = new JFrame("Chat");
+	private JTextArea textArea=new JTextArea(5,30);
+	private JScrollPane scrollPane = new JScrollPane(textArea);
+	private JButton send =new JButton("Send");
+	private JTextArea reply = new JTextArea(5,30);
+	
+	public Chat(){
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		scrollPane.setPreferredSize(new Dimension(300, 300));
+		textArea.setText(Conv());
+		frame.add(scrollPane, BorderLayout.NORTH);
+		frame.add(reply, BorderLayout.CENTER);
+		
+		send.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				addReply();
+			}
+		});
+	
+		reply.addKeyListener(new KeyListener(){
+			@Override
+            public void keyTyped(KeyEvent e) {
+            }
+			 @Override
+	            public void keyPressed(KeyEvent e) {
+	                if ((e.getKeyCode() == KeyEvent.VK_ENTER) && ((e.getModifiers() & KeyEvent.CTRL_MASK) != 0)) {
+	                   addReply();
+	                }
+	            }
+			 @Override
+	            public void keyReleased(KeyEvent e) {
+	            }
+		});
+		
+		frame.add(send, BorderLayout.SOUTH);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
 	public static void main(String[] args){
 		
-		Students.add(new Student("Nathan", "Borup", 1));
-		Students.add(new Student("Ethan", "Brown", 1) );
-		Students.add(new Student("Michael", "Cullimore", 1) );
-		Students.add(new Student("Kendra", "Koester", 1) );
-		Students.add(new Student("Trevor", "Marsh", 1) );
-		Students.add(new Student("Cody", "May", 9999) );
-		Students.add(new Student("Brieanna", "Miller", 1) );
-		Students.add(new Student("Rizwan", "Mohammad", 1) );
-		Students.add(new Student("Lauren", "Ribiero", 1) );
-		Students.add(new Student("Tyler", "Toponce", 1) );
-		for(Student student : Students){
-			System.out.println(student.getFirstName());
-		}
+		students.add(new Student("Brieanna", "Miller", 1) );
+		students.add(new Student("Nathan", "Borup", 1));
+		students.add(new Student("Ethan", "Brown", 1) );
+		students.add(new Student("Tyler", "Toponce", 1) );
+		students.add(new Student("Kendra", "Koester", 1) );
+		students.add(new Student("Cody", "May", 9999) );
+		students.add(new Student("Michael", "Cullimore", 1) );
+		students.add(new Student("Rizwan", "Mohammad", 1) );
+		students.add(new Student("Lauren", "Ribiero", 1) );
 		
-		Groups.add(new Group (Students.get(0), Students.get(1)));
-		Groups.add(new Group (Students.get(2), Students.get(3)));
-		Groups.add(new Group (Students.get(4), Students.get(5)));
-		Groups.add(new Group (Students.get(6), Students.get(7)));
-		Groups.add(new Group (Students.get(8), Students.get(9)));
+		students.add(new Student("Trevor", "Marsh", 1) );
+		
+		Collections.sort(students, new NameComp());
+		
+		
+		groups.add(new Group (students.get(0), students.get(1)));
+		groups.add(new Group (students.get(2), students.get(3)));
+		groups.add(new Group (students.get(4), students.get(5)));
+		groups.add(new Group (students.get(6), students.get(7)));
+		groups.add(new Group (students.get(8), students.get(9)));
 		int counter=0;
-		for(Student student : Students){
+		for(Student student : students){
 			if(counter==0){
 				counter++;
-				student.Responses.add(new Response("Hello"));
-				student.Responses.add(new Response("What's up?"));
-				student.Responses.add(new Response("Everything is awful"));
-				student.Responses.add(new Response("I agree."));
-				student.Responses.add(new Response("I am a dragon, grrr"));
+				student.responses.add(new Response("Hello"));
+				student.responses.add(new Response("What's up?"));
+				student.responses.add(new Response("Everything is awful"));
+				student.responses.add(new Response("I agree."));
+				student.responses.add(new Response("I am a dragon, grrr"));
 				}
 			else{
 				counter=0;
-				student.Responses.add(new Response("Hey"));
-				student.Responses.add(new Response("Trying to live my life"));
-				student.Responses.add(new Response("What?"));
-				student.Responses.add(new Response("I'm not following..."));
-				student.Responses.add(new Response("Please leave"));
+				student.responses.add(new Response("Hey"));
+				student.responses.add(new Response("Trying to live my life"));
+				student.responses.add(new Response("What?"));
+				student.responses.add(new Response("I'm not following..."));
+				student.responses.add(new Response("Please leave"));
 			}
 			
 		}
-		for(Group group : Groups){
-			Student one= group.Members.get(0);
-			Student two= group.Members.get(1);
-			System.out.println("Conversation between " + one.getFirstName()+ " " +one.getLastName()+ " and " + two.getFirstName()+ " " +two.getLastName()+ ":");
-			for(int i=0; i<5; i++){
-				System.out.println(one.getFirstName()+ " " +one.getLastName()+ " says:");
-				System.out.println(one.Responses.get(i).getResponse());
-				System.out.println(two.getFirstName()+ " " +two.getLastName()+ " says:");
-				System.out.println(two.Responses.get(i).getResponse());
+		new Chat();
+		
+	}
+		private String Conv(){
+			String conv="";
+			for(Group group : groups){
+				Student one= group.selStudent(0);
+				Student two= group.selStudent(1);
+				conv=(conv +"Conversation between " + one.getFirstName()+ " " +one.getLastName()+ " and " + two.getFirstName()+ " " +two.getLastName()+ ":\n");
+				for(int i=0; i<5; i++){
+					conv=(conv +one.getFirstName()+ " " +one.getLastName()+ "\n");
+					conv=(conv +one.printResponse(i)+"\n");
+					conv=(conv +two.getFirstName()+ " " +two.getLastName()+ " says:\n");
+					conv=(conv +two.printResponse(i)+"\n");
+					
+				}
 				
 			}
-			
+			return conv;
 		}
+		
+		private void addReply(){
+			textArea.setText(textArea.getText()+ reply.getText()+"\n");
+			reply.setText("");
+		}
+
+		
 		
 		
 		
 		
 	}
+
+class NameComp implements Comparator<Student>{
+	@Override
+	public int compare(Student s1, Student s2) {
+		String name1=s1.getLastName();
+		String name2=s2.getLastName();
+		return name1.compareTo(name2);
+        
+    }
+
+	
 }
