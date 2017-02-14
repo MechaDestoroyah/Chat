5c317b02a2a7ec813eff2bf2e62e6989053220f2
MechaDestoroyah <codymay@mail.weber.edu>
2017-02-13 19:09:24 -0700

    Update Group.java

--- Group.java
+++ Group.java
@@ -3,11 +3,18 @@ package Chat;
 import java.util.*;
 
 public class Group {
-	public List<Student> Members = new ArrayList<Student>();
+	public List<Student> members = new ArrayList<Student>();
 	public Group (Student one, Student two){
-		Members.add(one);
-		Members.add(two);
+		members.add(one);
+		members.add(two);
 		
+	}
+	public Response getResponse(int stu, int pos){
+		Student sel = members.get(stu);
+		return sel.getResponse(pos);
+	}
+	public Student selStudent(int stu){
+		return members.get(stu);
 	}
 
 }
